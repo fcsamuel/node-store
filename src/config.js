e28b5abff54f75e9108d0c5f782bd169fc92b241
Felipe Carvalho Samuel <felipe.samuel@insidesistemas.com.br>
2022-02-08 15:08:05 -0300

    build: dados de configuração

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-global.SALT_KEY = 'value_here';
+global.SALT_KEY = '6e1bd31f-e85a-448e-a1ce-fc02f045d589';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>! Seja bem-vindo, esse é o email de teste.';
 
 module.exports = {
-    mongoConnString: 'value_here',
-    sendgridKey: 'value_here',
-    azureConnString: 'value_here',
-    fileShareName: 'value_here',
-    storageAccount: 'value_here'
+    mongoConnString: 'mongodb+srv://felipe_samuel:fcsroot01.@nodecluster.m2hiy.mongodb.net/node-app?retryWrites=true',
+    sendgridKey: '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',
+    azureConnString: 'DefaultEndpointsProtocol=https;AccountName=fcsnodestore;AccountKey=t/8m5jeHHLYfpSQkSuxHS1qcM9q4DRN7jVZfLnetc1+q8DwxUUM6kP9/q9Ckmlj/i75LvoCpehmU+AStlxgsSQ==;EndpointSuffix=core.windows.net',
+    fileShareName: 'node-store-files',
+    storageAccount: 'fcsnodestore'
 };
